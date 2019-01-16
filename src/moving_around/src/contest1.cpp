c4ee904b336f6e1204f4786a9cd2cbd569a866a3
ahsueh1996 <ahsueh1996@gmail.com>
2019-01-15 19:30:48 -0500

    attempt tf message listening

--- src/moving_around/src/contest1.cpp
+++ src/moving_around/src/contest1.cpp
@@ -3,7 +3,6 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
-#include <tf/transform_listener.h>
 #include <eStop.h>
 
 #include <stdio.h>
@@ -18,7 +17,7 @@ void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	//fill with your code
-	//cout<<*msg<<endl;
+	cout<<*msg<<endl;
 }
 
 int main(int argc, char **argv)
@@ -27,12 +26,8 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 	teleController eStop;
 
-	// Get bumper and kinect laser "interrupts"
 	ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
 	ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
-
-	// Use the tf channel for base_footprint pose estimates
-	tf::TransformListener listener;
 
 	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
@@ -41,17 +36,13 @@ int main(int argc, char **argv)
 	geometry_msgs::Twist vel;
 
 	while(ros::ok()){
-		tf::StampedTransform transform;
-		listener.lookupTransform("/base_footprint","/map",ros::Time(0), transform);
-
-		
-
 		ros::spinOnce();
 		//.....**E-STOP DO NOT TOUCH**.......
 		eStop.block();
 		//...................................
 
 		//fill with your code
+		cout<<"spin"<<endl;
   		vel.angular.z = angular;
   		vel.linear.x = linear;
 
